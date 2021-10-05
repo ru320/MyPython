8b1bed1181987926763d3ea8b51f64ad2f7c8629
Ru320 <r.fichtenau@outlook.de>
2021-10-05 23:55:04 +0200

    Combobox und Save

--- abaqus/evmadi/EVMADI.py
+++ abaqus/evmadi/EVMADI.py
@@ -35,7 +35,8 @@ from io import BytesIO
 Version = '1.0'
 data = 'R0lGODlhlgAsAPcAABkXF/WdO5+dnfvPn1VTU9HPz/3p0be3tXNzcTU1M+vp6f316fevX6urqd3d22VjY/f19UVFQyknJ8PDwYGBf42Ni/nFif/79ZWVk6Wlo9fX1bGxr9/d3V1bWx8fH3t7ez89Pfm1b21ra09NTcnJx+Pj4f3t3b+9ve/v7S8vLYeHhf37+/X185mZl6Ghn6+vrWdnZcfHxdvb2V9fXf3v3f3fvdPT0b27uTs5OUtJSYWFg5GRj6mpp7W1syUjI39/ffm7d+fn5R0bG1dXVXd3de3t6/e1a6+trfn59y0rK8fFxfvLlfv7+dvZ2eHh30FBP1NRUfPz8TMzM////Wtpaf3z57u5uf/17a2tq0dHR8XFw4ODg5mXl6mnp9nZ17Wzs319e3Fvb09PTc3Ny+fl5cHBv/Px8Y2Li/39+//58Zubm6Wjo2lpZ2NhYfm9exkZGZ+fnfvVq9HRz/3r17m3tzk3N+vr6f/166urq/f39SkpKcXDw5GPj5WVlaenpdnX17Ozsf3n0f3ly+np5/vZsf/37/3hw/vNm83LyzMxMYuJidfV1ZWTk1tZWXt5eUVDQ/3x4SMhIVdVVWdlZY+Pjd/f3V1dW0E/P21ta+Xl4/Hx76OjoWFhX9XV0zs7OU1LSyUlIx8dHfm3bS8tLfm9ef/9+f3t2fm3b7+/vXVzczc1NUdFRYOBgaelpbOxseXj4/Hv7zEvL4mHh5uZmaOhobGvr2lnZ8nHx93b22FfX4eFhZORkaupqbe1tYF/f+nn51lXV3l3d+/t7fv5+ePh4UNBQX99fVFPT8/NzcPBwf/9/aGfn9PR0e3r6/n39+Hf319dXW9tbT07OyclJfWdPfevYfnFi//79/3v3727u/vLl//7+f/58/m9fbm3tf/16cXDwdnX1SEfH317e/m1azUzM//z50lHR4WDg1FPTf/9+52bm2tpZxsZGa2rqyspKZeVlQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAAAAAAALAAAAACWACwABwj/AJVNEYhmoMGCBA8aTCiQocKGUxA+jEgRokSHGCtOtLhQY8aLHkOCHNlR4kaRJVNyXHmSJMuPKhW6bBkzI8yXHnGSlFiQp8aeMoPqrIlyqM+jQikiHcp0CtGZN2kafUq1adSrSqtKBZpwqdSvML1yzdo1KVaoRZOOXfvT6U6wac+SncvWJ06IZVm+zau2rVdlTcfKnWo1bki8HfkiRrwX7syJCBOmFOu3L9rLVP9W5ivYMFrALQsBpjyzblDSRAVOiZLHs1a5qDk75cskRAAgil0XZssR6g1x0xAlTtu5tG64OE0EWH4FJOPhuw9bVqhuii0AAH5Mpkn5rmnZPmNe/2MQwEgp8NPRR8dZS0ioPdJzx544ezvW3geZ0GCymO5mx/9hRsYgJzlUXEx1gdaZehT11R9hbTXlFnQNqmYXhSClpOFQTtXXmHPDXYSGMisoOBcLKCBBn0CwaGJfgygigcZYFUYECwpoVGcSUAVVB5oZKDAR0UprcUSWOrBEMWNHU9y4AkoZ5qXMHCGEYEQVBE2SCydMABJBOwCE0gEHFq2wQyzY1bHJFOjk0oYMFZWyzCVghgKFFj4OpEQbuTDCAiujYJeELkIShEorGdyiURS6JIKdDzBU4uEDnHCCjFJ85JILLQgxwUkbbaBxAA4AtCPJIAShYEygAEwjwiuGvf8kyHLUYEORENidASZ22CXAgkHqfMArr2dEgB0JBq0Aw7DYCdEDRLxgB8wIzAJgDFDQYJfKQigUU60EMkCEKwB0SNQBdkT0xASvgOyKXQcD/UIqs6OUcJeHFSkz63LYEBTKsFKMoAevXTTUya7ifAJCqaBgJ5xAy2D3xieT1CFoFA3hMWw7j+QwrjhR+JgtAKlIJAJ2HgyRSxLYQXHQuAcsdC4ARDS0rqAA1DGJJ2+4MIU6M2AnTgfQSIAdJ/hB5Je+ywVgq0DjtpPBkyikgB0CFFEg6DMFnRAJr4j0pAoAb7gyEBPXAbCGQdGm6UVBrvAaQ0Mjp0JQJmBOo0FE3WL/t3dBMFc0c80R3ZyrkHm0Up0XYI4iKRqDWPwGrLmhtK/T/mKHTkVhYDdEQ1lgV0FCaoA9kAbYEWDQFCW8AcADs2kMgBCLGMSEONjxYFDdBq2BnQqqTbEBdhgYNC4dSg1OkOHwAlUdI8Q36AJ2rRAlFNP8Go9dD0Chg90IEaHhA3ZaJITErmFP0TYl4c9Iah2gtS0BT44CIABBdSNEBHbJDKmOHa6jgvYAcICEDK5TvOrFcNiAHWYMZES4wI4jCjQhjlyuXxH5FwB6kRDvAUAMBTGD6wBgL6WM62GUwE4tOuSURoQJIbKTQFfGBoB1UCR/A7EE2WDFQqOJgSCBQ4jy/wrHK1TwKCLUAkAUWMgEXOXiLs/BXq1utb2K6OB7EQkCr2CRFfQVhBXYicQ0xjhGDWJMfdiZn1JoKACE8E4gYsAOKMg4RjBdYoAxa8gQ0WC4E2SlIJfADh3H6DrwVY4vVSCEIgsxwGdRxIM/ZJ3ENNGVE0bEGNVilh0GEkOFlAM794tI3RqSxEwCQBpUJKDM0LW8Ii7mCabEThbMUpM7VGEOc7hA5ja4kCt+sCDN4NUvukg2ZClDFtgJAxeWycxlskAg8uMKDW1YEN4VhAC5akEztemHVJbLIAc8mysfYqw3YICZ2lzmEWiSFEPEwQ3WqEIju4edSJ4PO3JIiDpOWP+QFoASPA1Io9I+ab+e4FAZaWOGWI53kD0aDhWSicgQsPM4Wl4II9g4hEYZCbgqNsSXIBwIQTchkRiM8GEnwA4m+iK/fLGRbtoiiCKwAweu+ESDL6jIJ1hJROxAdCCAqY6wAHCEi04oPHwxwRKWykiBaFCBBWFTPRHCCexEQEVTKMLCHFaKKaDgX5EogUGQgIC9NSSgAJDA6qZA0FBO4Y1TSAZ2iqEigWQiDHaI6hRYBQaKCIBXhFPGQ4PXoS98jz9OcQARKPlAZYwIQwvhhgkCgQ0hzWhcjpyRL8FXkLYBIAd8QEf9HGaQSWBHFcuYQC2OAQBxPIMi0WzfNMM3ysL/WQwAI8DCBNZgsTrMaEaSEBorZlFVwKqriCP67YxYwCpJbGAPAnDUKhASCAZcKStJwYYRLBACbnRIg450igfBFxEkBJJZBDhhV5VRApZVSwR6jW2HCGpDp+DQKahw17CWwULfMYtjPJ3CMIrIQhbGrVrtcMdsgLCcAYAGqBsxRDcOYYRvAEYZkQhFKOhw4WNqGAoKysQ5hsUGTViyITLYKa9CwYphdBgLGh5Fh5WBAw0v48Jt0HAwZowKGmLnHa2YMRNOxisJ8GAWGnbEhYeh4VBAdMYX/sJoATCKWnR4CcupAVBVwxdu0MAE2OhqgcdcYIKgARF+eAEHInLi1f3h/wgZyIYwyEznOtN5BYjgRRe0AAE6NwELrUjGr+xM6CkggQRd4EEMsNqhUtRAENXpkFGUS+lKK3d1F7knABbBI0t7+tOgDrWoR03qUtfHQIWus5k1woFdBQEoqY61rGdN61qT+S9QzrWuI1IALjxwIH4Q5GN1TexiG/vYyE62souNL5PY2i1cEIIQbuAWO8yrDQ16tra3zW1ZX6jUn56CFZr1AD6AgVUAKEP7wM3udrv73aE+dba5jYaZVYsKeu22vvfN76PAu9JREIF+Q0EBGf374AhPeLwVwu8OOYEHjIAHIIrQ8IpbXNt4WbbGN87xjnv848euz8VHTvKSz7ohIBBPucpXzvKPm/zlMI95gQMCADs='
 if myVersion[0] == '3':
-    image_64_decode = base64.decodebytes(data) 
+    image_64_decode = base64.b64decode(data) 
+    # image_64_decode = base64.decodebytes(data) 
 
 if myVersion[0] == '2':
     image_64_decode = base64.b64decode(data) 
@@ -51,7 +52,7 @@ Result_row = 1
 
 #- Gui Variables
 BTN_Dict = {}
-Result_Title = ['Name', 'Step', 'Time', 'Result', 'Resulttype', 'View', 'Legend Min' , 'Legend Max' ,'Legend']
+Result_Title = ['Load','Name', 'Step', 'Frame', 'Result', 'Resulttype', 'View', 'Legend Min' , 'Legend Max' ,'Legend']
 Result_Dict  = {}
 
 #- Dummy Dict
@@ -61,14 +62,21 @@ def dummy_Dict():
     global Result_row
     Local_Dict = {}
     for myItem in Result_Title:
-        Local_Dict[myItem ] = myItem
+        Local_Dict[myItem ] = myItem + str(Result_row)
+    Local_Dict['ID'] = Result_row
     Result_Dict[str(Result_row)] = Local_Dict
+    Result_row += 1
 
+dummy_Dict()
 dummy_Dict()
 
 #- Close GUI
 def Close():
     root.destroy()
+
+#- Load Result
+def Load(Local_row):
+    print('Load Result' , Local_row)
 
 #- Choose File GUI
 def StoreResult():
@@ -92,7 +100,14 @@ def LoadSettings():
 
 #- Save Settings
 def Save():
-    print('Save ' + myFile)
+    global myFile
+    mySaveFile = tk.filedialog.asksaveasfile(title = "Save XML file for EVMADI",mode='w', defaultextension=".xml")
+    if mySaveFile is None: # asksaveasfile return `None` if dialog closed with "cancel".
+        print('Abbruch')
+        return
+    myFile = mySaveFile.name
+    print('Save ' + str(myFile))
+     
 
 #- Remove Row
 def RemoveRow(FrameName,Local_row,Local_col):
@@ -102,10 +117,14 @@ def RemoveRow(FrameName,Local_row,Local_col):
     btn.destroy()
 
 #- Add View Row
-def AddRow(Local_Dict):#Name, Step, Time, Rsult, Resulttype, View, Legend_Min , Legend_Max ,Legend):
+def AddRow(Local_Dict):#Name, Step, Frame, Rsult, Resulttype, View, Legend_Min , Legend_Max ,Legend):
     global Result_row
     global Result_Dict
     myCol = 0
+    #- Load
+    BTN_Load = tk.Button(Results_Frame, text='Load', command=lambda:Load(Local_Dict['ID']), font='AvenirNextCondensed',image=pixelVirtual,compound="c",width = 10,height=8)
+    BTN_Load.grid(column=myCol,row=Result_row)
+    myCol +=1
     #- Name
     Name = Local_Dict['Name']
     TXT_Name = tk.Text(Results_Frame,width=10,height=1)
@@ -118,11 +137,11 @@ def AddRow(Local_Dict):#Name, Step, Time, Rsult, Resulttype, View, Legend_Min , 
     TXT_Step.insert(tk.END,Step)
     TXT_Step.grid(column=myCol,row=Result_row)
     myCol +=1
-    #- Time
-    Time = Local_Dict['Time']
-    TXT_Time = tk.Text(Results_Frame,width=10,height=1)
-    TXT_Time.insert(tk.END,Time)
-    TXT_Time.grid(column=myCol,row=Result_row)
+    #- Frame
+    Frame = Local_Dict['Frame']
+    TXT_Frame = tk.Text(Results_Frame,width=10,height=1)
+    TXT_Frame.insert(tk.END,Frame)
+    TXT_Frame.grid(column=myCol,row=Result_row)
     myCol +=1
     #- Result
     Result = Local_Dict['Result']
@@ -132,9 +151,12 @@ def AddRow(Local_Dict):#Name, Step, Time, Rsult, Resulttype, View, Legend_Min , 
     myCol +=1
     #- Resulttype
     Resulttype = Local_Dict['Resulttype']
-    TXT_Resulttype = tk.Text(Results_Frame,width=10,height=1)
-    TXT_Resulttype.insert(tk.END,Resulttype)
-    TXT_Resulttype.grid(column=myCol,row=Result_row)
+    CMB_Resulttype = tkinter.ttk.Combobox(Results_Frame,width=10,height=20,values=["Contour","Symbols"])
+    CMB_Resulttype.grid(column=myCol,row=Result_row)
+    if Resulttype == "Symbols":
+        CMB_Resulttype.current(1)
+    else:
+        CMB_Resulttype.current(0)
     myCol +=1
     #- Vieworientation
     View = Local_Dict['View']
@@ -187,7 +209,8 @@ pixelVirtual = tk.PhotoImage(width=1, height=1)
 #- Logo
 Logo_Frame = tk.Frame(root, bg='white', width=450, height=50, pady=3)
 Logo_Frame.grid(column=0,row=0)
-Logo = tk.PhotoImage(file= 'B:\\temp\\DI_Logo.gif')
+Logo = tk.PhotoImage(file= 'DI_Logo.gif')
+# Logo = tk.PhotoImage(file= 'B:\\temp\\DI_Logo.gif')
 Logo_Label = tk.Label(Logo_Frame,image=Logo,bg='white')
 Logo_Label.grid(column=1,row=0)
 
